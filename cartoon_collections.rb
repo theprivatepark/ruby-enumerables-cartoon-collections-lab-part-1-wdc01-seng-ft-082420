12a7a9de73863faf09994d2977d3c609249e2f0f
Anney Park <anneypark@gmail.com>
2020-07-28 21:03:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |name|
-    puts name
+    pu name
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
