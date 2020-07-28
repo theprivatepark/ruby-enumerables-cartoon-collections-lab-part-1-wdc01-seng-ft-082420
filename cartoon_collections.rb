9094aeb240edd23883cbc9e195841a24b6c72110
Anney Park <anneypark@gmail.com>
2020-07-28 21:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index { |item, index| item item = 1 
+  array.each_with_index { |item, index| item = 1 
   p "#{index}.#{item}"}
   # Use `each_with_index` to enumerate over the provided array
   #
